429c7072209ac3275c3f702faf317ab556300fb0
plonkamaciej <91895074+plonkamaciej@users.noreply.github.com>
2024-05-13 14:25:59 +0200

    fix ticket prices and passes calculation fix api call to skischedule

--- Frontend/src/components/Admin.tsx
+++ Frontend/src/components/Admin.tsx
@@ -5,9 +5,9 @@ import { Button } from "./ui/button";
   export function AdminComp() {
     return (
         <div className="grid grid-cols-2 gap-4 lg:flex lg:flex-row">
-          <OrderCard title="Harmonogram">
-            <p className="mb-4">see all harmonograms, you can delete and add new ones here</p>
-            <a href="/admin/adminHarmonogram"><Button>Harmonogram</Button></a>
+          <OrderCard title="Schedule">
+            <p className="mb-4">see all schedules, you can delete and add new ones here</p>
+            <a href="/admin/adminHarmonogram"><Button>Schedule</Button></a>
           </OrderCard>
           <OrderCard title="Prices">
           <p className="mb-4">Change prices of the tickets and passes here</p>
