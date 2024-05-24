b3b64059f19493ff717fd5d2eb57a64a01f78844
Herkules09 <mk301788@student.polsl.pl>
2024-05-24 10:12:46 +0200

    add filtering reports by orderDate

--- Backend/SkiStation/src/main/java/org/skistation/controllers/ReportController.java
+++ Backend/SkiStation/src/main/java/org/skistation/controllers/ReportController.java
@@ -1,5 +1,6 @@
 package org.skistation.controllers;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,10 +12,8 @@ import org.skistation.models.Order;
 import org.skistation.models.Pass;
 import org.skistation.models.Ticket;
 import org.skistation.services.*;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -55,6 +54,39 @@ public class ReportController {
                 report.totalTicket().add(ticketTotal);
             }
 
+        }
+
+        return new ArrayList<>(reportMap.values());
+    }
+
+
+    @GetMapping("/byDate")
+    public List<ReportDTO> getReportsByDateRange(
+            @RequestParam("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endDate) {
+
+        List<Object[]> rawReports = reportService.getReportsByDateRange(startDate, endDate);
+        Map<Integer, ReportDTO> reportMap = new HashMap<>();
+
+        for (Object[] row : rawReports) {
+            Integer orderId= (Integer) row[0];
+            String email = (String) row[1];
+            Integer phone = (Integer) row[2];
+            Float orderTotal = (Float) row[3];
+            Float passTotal = (Float) row[4];
+            Float ticketTotal = (Float) row[5];
+
+            ReportDTO report = reportMap.get(orderId);
+            if (report == null) {
+                report = new ReportDTO(orderId,email, phone, orderTotal, new ArrayList<>(), new ArrayList<>());
+                reportMap.put(orderId, report);
+            }
+            if (passTotal != null) {
+                report.totalPass().add(passTotal);
+            }
+            if (ticketTotal != null) {
+                report.totalTicket().add(ticketTotal);
+            }
         }
 
         return new ArrayList<>(reportMap.values());
