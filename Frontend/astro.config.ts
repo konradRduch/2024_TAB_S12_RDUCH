b85a2dc4e93f91524cc4791934a944d267152aa6
konradRduch <91778942+konradRduch@users.noreply.github.com>
2024-05-31 18:40:29 +0200

    docker

--- Frontend/astro.config.ts
+++ Frontend/astro.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-})
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+});
