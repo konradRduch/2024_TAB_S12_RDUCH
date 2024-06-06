993fdb8e5f4b33fa8c9e38b3e675acd9f21fe0f6
GargamelJR1 <91876652+GargamelJR1@users.noreply.github.com>
2024-06-06 19:04:08 +0200

    config, main classes javadoc comments

--- Backend/SkiStation/src/main/java/org/skistation/SkiStationApplication.java
+++ Backend/SkiStation/src/main/java/org/skistation/SkiStationApplication.java
@@ -3,12 +3,18 @@ package org.skistation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class for the Ski Station application.
+ */
 @SpringBootApplication
 public class SkiStationApplication
 {
-
+    /**
+     * The main method that starts the Ski Station application.
+     *
+     * @param args the command-line arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(SkiStationApplication.class, args);
     }
-
 }
--- Backend/SkiStation/src/main/java/org/skistation/WebMvcConfig.java
+++ Backend/SkiStation/src/main/java/org/skistation/WebMvcConfig.java
@@ -4,8 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration for the web MVC.
+ */
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer{
+public class WebMvcConfig implements WebMvcConfigurer
+{
+    /**
+     * Adds CORS mappings.
+     *
+     * @param registry the CORS registry.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
